e38aae32feb040cf06a069bbc18c2a30aa560c6f
hung-tp.genki <hung-tp@genkisystem.com>
2022-01-18 19:22:23 +0700

    Remove initial todo

--- src/redux/todo_slice.ts
+++ src/redux/todo_slice.ts
@@ -5,21 +5,7 @@ interface TodoState {
 }
 
 const initialState: TodoState = {
-  todos: [
-    { name: "Todo 1" },
-    { name: "Todo 2" },
-    { name: "Todo 3" },
-    { name: "Todo 4" },
-    { name: "Todo 5" },
-    { name: "Todo 6" },
-    { name: "Todo 7" },
-    { name: "Todo 8" },
-    { name: "Todo 9" },
-    { name: "Todo 10" },
-    { name: "Todo 11" },
-    { name: "Todo 12" },
-    { name: "Todo 13" },
-  ],
+  todos: [],
 };
 
 export const todoSlice = createSlice({
